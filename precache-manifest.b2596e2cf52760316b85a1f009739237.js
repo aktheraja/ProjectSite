18652e7dad3eed94016a4528e277a80bf53d6bb7
aktheraja <wale.akin2007@gmail.com>
2019-11-19 19:54:30 -0700

    Updates

---
+++ precache-manifest.b2596e2cf52760316b85a1f009739237.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10e21df42f3b969a2c5d0cb3d753a7e",
+    "url": "/ProjectSite/index.html"
+  },
+  {
+    "revision": "662f8c110608f4359f5b",
+    "url": "/ProjectSite/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "fb444058e714e4224b49",
+    "url": "/ProjectSite/static/css/main.bddf5ecb.chunk.css"
+  },
+  {
+    "revision": "662f8c110608f4359f5b",
+    "url": "/ProjectSite/static/js/2.3e808b22.chunk.js"
+  },
+  {
+    "revision": "fb444058e714e4224b49",
+    "url": "/ProjectSite/static/js/main.25d2cac2.chunk.js"
+  },
+  {
+    "revision": "e08785152e8d7b22e9f0",
+    "url": "/ProjectSite/static/js/runtime-main.7f4deb62.js"
+  },
+  {
+    "revision": "9a6157c430b22b4847d1f5a750d028b6",
+    "url": "/ProjectSite/static/media/1.9a6157c4.jpg"
+  },
+  {
+    "revision": "abab4722db10803187ac8bbe261b7692",
+    "url": "/ProjectSite/static/media/cloud.abab4722.jpg"
+  },
+  {
+    "revision": "5cd94d94ae8e08216d0ddfd865b2477c",
+    "url": "/ProjectSite/static/media/devops.5cd94d94.jpg"
+  },
+  {
+    "revision": "4216c4bcef5b1a39b7eb4f3ad421b9d7",
+    "url": "/ProjectSite/static/media/opeartions.4216c4bc.jpg"
+  }
+]);
