75abdb5aef88473f0bcebcd6cebf62e938f44616
aktheraja <wale.akin2007@gmail.com>
2019-11-22 08:02:00 -0600

    contact page added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,11 +1,52 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
+import React, {Component} from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-const project =()=>{
-    return (
-        <Container className=" contact_css">
-        <div><h1>This is the contact page</h1></div>
-        </Container>
-    )
-};
-export default project;
+class Contact extends Component{
+    render() {
+        return(
+<div className="container-fluid">
+    <div className="container">
+            <Row>
+                <div className="inquiry">
+                <Col >
+                    <Form>
+                        <Form.Group controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Enter Name"/>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Email" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicSubject">
+                            <Form.Label>Subject</Form.Label>
+                            <Form.Control type="subject" placeholder="Enter Subject" />
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Control as="textarea" rows="3" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+
+                </Col>
+                </div>
+
+                <div>
+                <Col>2 of 2</Col>
+                </div>
+            </Row>
+</div>
+</div>
+        )
+    }
+}
+export default Contact;
